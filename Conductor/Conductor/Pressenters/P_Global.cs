3043866bd835c5edea8e71b7abccf545be2cee8e
Mamory <32417036+Drift2020@users.noreply.github.com>
2017-12-13 14:35:54 +0200

    FerstCommit

---
+++ Conductor/Conductor/Pressenters/P_Global.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conductor
+{
+    class P_Global
+    {
+        private readonly I_Global _viwe;
+        private readonly 
+        public P_Global(I_Global viwe)
+        {
+            _viwe = viwe;
+        }
+    }
+}
